1cf479254d840e7669b92fe4e42e44e97b57d248
Matthew Wu <matthew.e.wu@jpl.nasa.gov>
2016-07-21 10:02:20 -0700

    Change color of artist container li on click

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 
-// Component decorator 
-/* Angular replaces the inner html of <my-app> with the specified template 
+// Component decorator
+/* Angular replaces the inner html of <my-app> with the specified template
  * at the file url */
 @Component({
     selector: 'my-app',
@@ -11,8 +11,9 @@ export class AppComponent {
     name = 'Ray';
     artists = ['Barot Bellingham', 'Jonathan Ferrar', 'Hillary Post'];
 
-    onClick(myName) {
+    onClick(myName, myElement) {
         this.name = myName;
+        myElement.style.backgroundColor = "#FECE4E";
     }
 
     addArtist(myArtist) {
