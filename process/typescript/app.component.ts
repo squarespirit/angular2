64fa623889f9ca0fd8cd3af30487808abe1bc1af
Matthew Wu <matthew.e.wu@jpl.nasa.gov>
2016-07-21 09:26:26 -0700

    Create and display list of artists

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -8,10 +8,21 @@ import {Component} from 'angular2/core';
     templateUrl: 'partials/app.html'
 }) // No semicolon
 export class AppComponent {
-    // Instance variable
+    // Instance variables
     name: string;
+    artists: any;
 
     constructor() {
         this.name = 'Bob';
+        this.artists = [
+            {
+                name: 'Barot Bellingham',
+                school: 'Penn State'
+            }, 
+            {
+                name: 'Hillary Post',
+                school: 'University of Illinois'
+            }
+        ];
     }    
 }
