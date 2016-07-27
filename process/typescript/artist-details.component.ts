eb2eda3949f30932812bbe5067c54f361d85cbf2
Matthew Wu <matthew.e.wu@jpl.nasa.gov>
2016-07-27 15:29:10 -0700

    Cleanup styles
    Move artist-item styles to its own css file
    Create css file for artist-detail styles

--- process/typescript/artist-details.component.ts
+++ process/typescript/artist-details.component.ts
@@ -1,11 +1,11 @@
 import {Component, Input} from 'angular2/core';
 import {Artist} from './artist';
 
-// Component decorator
 @Component({
   selector: 'artist-details',
-  templateUrl: 'partials/artistdetails.html' //inputs: ['artist']
-}) // No semicolon
+  templateUrl: 'partials/artistdetails.html',
+  styleUrls: ['css/app-details.css']
+})
 export class ArtistDetailsComponent {
   @Input() artist: Artist;
 }
--- process/typescript/artist-item.component.ts
+++ process/typescript/artist-item.component.ts
@@ -4,18 +4,7 @@ import {Artist} from './artist';
 @Component({
   selector: 'artist-item',
   templateUrl: 'partials/artistitem.html',
-  styles: [`
-    img {
-      display: block;
-      float: left;
-      width: 60px;
-      -webkit-border-radius: 12px;
-      border-radius: 12px;
-      margin-right: 10px;
-      margin-bottom: 10px;
-    }`,
-    'h2 { margin-top: 0; }'
-  ]
+  styleUrls: ['css/app-item.css']
 })
 export class ArtistItemComponent {
   @Input() artist: Artist;
